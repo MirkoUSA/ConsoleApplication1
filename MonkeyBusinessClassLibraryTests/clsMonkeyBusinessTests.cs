6177e543a38e31305285c2911c2c7db2ae8e6a05
MirkoUSA <MirkoUSA@US-MIRKO>
2017-10-12 21:30:52 -0700

    Created Unit-Testing and renamed Class file name from original Class1

---
+++ MonkeyBusinessClassLibraryTests/clsMonkeyBusinessTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MonkeyBusinessClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyBusinessClassLibrary.Tests
+{
+    [TestClass()]
+    public class clsMonkeyBusinessTests
+    {
+        [TestMethod()]
+        public void XingMonkeysTest()
+        {
+            var cls = new clsMonkeyBusiness();
+
+            // Use this to test the response with the dynamic parameters
+            var objResponse = cls.XingMonkeys(10, 5);
+
+            Assert.Fail();
+        }
+    }
+}
