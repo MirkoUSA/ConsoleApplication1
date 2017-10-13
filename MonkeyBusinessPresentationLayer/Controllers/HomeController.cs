016e7eb30825b448aed0dbb95702b6eb00674b0e
MirkoUSA <MirkoUSA@US-MIRKO>
2017-10-12 21:57:58 -0700

    Added logic to bind Class Response to model in the controller Getting ready to build Front-End

--- MonkeyBusinessPresentationLayer/Controllers/HomeController.cs
+++ MonkeyBusinessPresentationLayer/Controllers/HomeController.cs
@@ -4,13 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using MonkeyBusinessClassLibrary;
+using MonkeyBusinessPresentationLayer.Models;
+
+
 namespace MonkeyBusinessPresentationLayer.Controllers
 {
+    /// <summary>
+    /// Reusing the Index module to save time
+    /// </summary>
+
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        // Commented out original Index method
+        //public ActionResult Index() { return View(); }
+
+        public ActionResult Index(MonkeysViewModel model)
         {
-            return View();
+            // Crate new Model if parameter is null
+            if (model == null) { model = new MonkeysViewModel(); }
+
+            // Initiate the Class & assign results to model
+            var cls = new clsMonkeyBusiness();
+            model.CrossingMonkeys = cls.XingMonkeys(model.LeftValue, model.RiteValue);
+
+            return View(model);
         }
 
         public ActionResult About()
