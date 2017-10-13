fff4351ee8c41ade3859d08131a325dd574673f0
MirkoUSA <MirkoUSA@US-MIRKO>
2017-10-12 21:44:54 -0700

    Created new ViewModel and added reference to the ClassLibrary

---
+++ MonkeyBusinessPresentationLayer/Models/MonkeysViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace MonkeyBusinessPresentationLayer.Models
+{
+    /// <summary>
+    /// Created ViewModel to use in the front-end 
+    /// </summary>
+
+    [DataContract]
+    public class MonkeysViewModel
+    {
+        [DataMember]
+        public int LeftValue { get; set; }
+        [DataMember]
+        public int RiteValue { get; set; }
+
+        [DataMember]
+        public string[] CrossingMonkeys { get; set; }
+    }
+}
